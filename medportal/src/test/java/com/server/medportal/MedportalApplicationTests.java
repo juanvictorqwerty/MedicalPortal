b5551a74a5fb6ed25e7c5d859347d0d29682be3c
Juan <juanvictorqwerty@gmail.com>
2026-04-10 19:50:09 +0100

    build(backend): migrate from gradle to maven build system and rename module to medportal
    Remove the entire Gradle-based backend module including source code, configuration files, and build scripts. Introduce new Maven-based medportal module with initial pom.xml and directory structure.

---
+++ medportal/src/test/java/com/server/medportal/MedportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.medportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ medportalserver/src/test/java/com/server/medportalserver/MedportalserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.medportalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedportalserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
